f627cf92eec7a8fb3c4d1ad5771f119e758a37b4
Gio Urnau <giovaniurnau9@gmail.com>
2019-09-12 22:09:55 -0300

    Primeiros arquivos do projeto

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+const request = "https://api.hgbrasil.com.br/finance?format=jason&key=f4e857c0";
+
+main(){
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Container(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
