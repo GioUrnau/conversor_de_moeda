9688255ee8c1c13a46536dda1c70cd25a073f49a
Gio Urnau <giovaniurnau11@gmail.com>
2019-09-12 22:53:44 -0300

    Ajsute de teste branch

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:async/async.dart';
-const request = "https://api.hgbrasil.com.br/finance?format=jason&key=f4e857c0";
+
+const request = "https://api.hgbrasil.com/finance?format=jason&key=f4e857c0";
 
 main() async {
   http.Response response = await http.get(request);
+  print(response.body);
 
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
